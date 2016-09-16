11d3d09726d8b2680c09bc9d9d59982944c68fd2
Lucas <followmyego@gmail.com>
2016-09-16 02:11:18 -0700

    User Profile: Added the layout section for the "About Me" as well as the layout section and code logic for the instagram images section.

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="33f7c381-f984-42c8-885f-243adccf43ca", modulePackage="net.egobeta.ego", sdkRoot="C:\\Users\\Lucas\\AppData\\Local\\Android\\sdk",layoutInfoDir="C:\\Users\\Lucas\\Desktop\\Ego_Test\\ego\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=17,enableDebugLogs=false,printEncodedError=true)
+@BindingBuildInfo(buildId="4140019d-3611-4807-959e-ab68178aef19", modulePackage="net.egobeta.ego", sdkRoot="C:\\Users\\Lucas\\AppData\\Local\\Android\\sdk",layoutInfoDir="C:\\Users\\Lucas\\Desktop\\Ego_Test\\ego\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=17,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
